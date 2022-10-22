62543710c198aca4fed294d9373141def6acfe0f
Shafekul Abid <abidshafee@users.noreply.github.com>
2022-10-23 00:01:49 +0600

    init cngs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import abid from "../assets/ShafekulAbid.png"
 //import NavBar from "./NavBar";
 
 // export default function Home() {
@@ -8,10 +9,25 @@ import React from "react";
 const Home = () => {
     return (
         <div name="home"
-        className ="h-screen w-full pt-20 bg-gradient-to-b from-slate-800 via-gray-800 to-black"
+        className ="h-screen w-full pt-20 bg-gradient-to-b from-slate-800 via-gray-800 to-black text-white"
         >
-            <div>
-            <h2 className="text-white text-xl">Portfolio Home</h2>
+            <div className="max-w-screen-lg mx-auto flex items-center 
+            flex-col justify-center h-full px-4 md:flex-row">
+                <div className="flex flex-col justify-center h-full">
+                    <h2 className="text-white text-4xl pr-4 sm:7xl font-bold">Hello There! Me Shafekul Abid</h2>
+                    <p className="py-4 max-w-md pr-4">
+                        I am working with Software Development Techologies for web and mobile devices such as with React-Native, React, Next, Django Sanity etc.
+                        I am also a Practitionar & Pationate about ML, DL, Data Science and Big Data Analytics. My prefered languges of choice are Python,
+                        JavaScript, and TSQL etc. Learn more about my work in the Portfolio section.
+                    </p>
+                    
+                    <div>
+                    <button>My Portfolio</button>
+                    </div>
+                </div>
+                <div>
+                <img src={abid} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:w-full"/>
+                </div>
             </div>
         </div>
     );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 
 const NavBar = () => {
+    const [nav, setNav] = useState(false)
 
     const links = [
         {
@@ -29,8 +30,8 @@ const NavBar = () => {
     ]
 
     return (
-        <div className="flex justify-between items-center w-full h-20 fixed 
-        bg-gradient-to-b from-black via-gray-900 to-blue-800 text-white px-4">
+        <div className="flex justify-between items-center w-full h-20 shadow fixed 
+        bg-gradient-to-b from-slate-900 to-slate-700 text-white px-4">
             <div>
                 <h1 className="text-5xl ml-3 hover:scale-105 duration-200 cursor-pointer">SA</h1>
             </div>
@@ -46,9 +47,23 @@ const NavBar = () => {
                     ))
                 }
             </ul>
-            <div className="cursor-pointer pr-3 z-10 text-white">
-                <FaBars size={30}/>
+            <div onClick={()=>setNav(!nav)} className="cursor-pointer pr-3 z-10 text-white md:hidden">
+                {nav? <FaTimes size={30}/>:<FaBars size={30}/>}
             </div>
+            {nav && (
+                <ul className="flex flex-col justify-center items-center
+                absolute top-0 left-0 w-full h-screen bg-slate-600 
+                text-white">
+                     {
+                        links.map(({id, link, name}) => (
+                            <li key={id} className="px-4 cursor-pointer capitalize 
+                            py-6 text-4xl">
+                                <Link to={link}>{name}</Link>
+                            </li>  
+                        ))
+                    }
+                </ul>
+            )}
         </div>
     )
 }
