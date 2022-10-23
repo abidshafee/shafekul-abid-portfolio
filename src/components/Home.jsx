efef920f96660a2c99e150fca76d557deb7b2599
Shafekul Abid <abidshafee@users.noreply.github.com>
2022-10-23 22:19:18 +0600

    init cngs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
             <div className="max-w-screen-lg mx-auto flex items-center 
             flex-col justify-center h-full px-4 md:flex-row">
                 <div className="flex flex-col justify-center h-full">
-                    <h2 className="text-white text-4xl pr-4 sm:7xl font-bold inline border-b-4 border-rose-700">Hello! Shafekul Abid here!</h2>
+                    <h2 className="text-white text-4xl pr-4 sm:7xl font-bold inline border-b-4 border-lime-500">Hello! Shafekul Abid here!</h2>
                     <p className="py-4 max-w-md pr-4">
                         I am working with Software Development Techologies for web and mobile devices such as with React-Native, React, Next, Django Sanity etc.
                         I am also a Practitionar & Pationate about ML, DL, Data Science and Big Data Analytics. My prefered languges of choice are Python,
@@ -23,8 +23,7 @@ const Home = () => {
                     
                     <div>
                     <button className="text-white w-fit px-6 py-3 my-2
-                    flex items-center rounded-md bg-gradient-to-r from-cyan-500 
-                    to-blue-500 cursor-pointer hover:scale-105 duration-300">Visit My Portfolio</button>
+                    flex items-center rounded-md bg-gradient-to-r from-lime-400 to-lime-600 cursor-pointer hover:scale-105 duration-300">Visit My Portfolio</button>
                     </div>
                 </div>
                 <div>
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,30 +1,58 @@
 import React from "react";
+import proj1 from "../assets/proj1.png";
+import proj2 from "../assets/proj2.png";
+import proj3 from "../assets/proj3.png";
+import proj4 from "../assets/proj4.png";
 
 export default function Project() {
+
+    const projects = [
+        {
+            id: 1,
+            src: proj1
+        },
+        {
+            id: 2,
+            src: proj2
+        },
+        {
+            id: 3,
+            src: proj3
+        },
+        {
+            id: 4,
+            src: proj4
+        }
+    ]
+
     return (
         <div name="Projects"
-        className ="bg-gradient-to-b from-black to-gray-800
-        w-full text-white md:h-screen"
+        className ="bg-gradient-to-b from-black to-gray-800 text-white"
         >
            <div className="max-w-screen-lg p-4 mx-auto 
-           flex flex-col justify-center w-full h-full">
+           flex flex-col w-full h-fit py-32">
                 <div className="pb-8">
                     <h2 className="text-4xl font-bold inline border-b-4 border-rose-700">My Projects</h2>
                     <p className="py-6">
                         Some of my project has been showcased here.
                     </p>
                 </div>
+             
 
-                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5
                 px-12 sm:px-0">
-                    <div className="shadow-md shadow-gray-700 rounded-lg">
-                        <img src="#" alt="p-imgs" className="rounded-md duration-200
+                       {
+                    projects.map(({id, src}) => (
+                        <div key={id} className="shadow-md shadow-gray-700 rounded-lg">
+                        <img src={src} alt="p-imgs" className="rounded-md duration-200
                         hover:scale-105"/>
                         <div className="flex items-center justify-center">
                             <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
                             <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
                         </div>
                     </div>
+                    ))
+                }                    
                 </div>
            </div>
         </div>
